37f2a5c8d87230425ce6c82aa3199ebfc8046c7d
Pavel Ven Gulbin <2tvenom@gmail.com>
2017-12-24 22:40:46 +0300

    git move bug

---
+++ kv/simple_cache.go
@@ -0,0 +1,258 @@
+package kv
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+	"sync"
+	"time"
+	"unsafe"
+	"log"
+)
+
+type (
+	simpleCacheDb struct {
+		blocks [blocks]map[string][]byte
+		locks  [blocks]sync.RWMutex
+	}
+)
+
+var (
+	_ = fmt.Printf
+)
+
+func newSimpleCacheDb() *simpleCacheDb {
+	c := &simpleCacheDb{}
+	for i := 0; i < blocks; i++ {
+		c.blocks[i] = map[string][]byte{}
+	}
+	return c
+}
+
+func (c *simpleCacheDb) Keys() []string {
+	out := []string{}
+	for i, block := range c.blocks {
+		c.locks[i].Lock()
+		for key := range block {
+			out = append(out, key)
+		}
+		c.locks[i].Unlock()
+	}
+	return out
+}
+
+func (c *simpleCacheDb) Remove(key string) {
+	id := blockByKey(key)
+	c.locks[id].Lock()
+	delete(c.blocks[id], key)
+	c.locks[id].Unlock()
+}
+
+func (c *simpleCacheDb) get(key string, keyType uint8) ([]byte, error) {
+	id := blockByKey(key)
+	c.locks[id].RLock()
+	log.Printf("Entry: %+v", c.blocks[id])
+	if data, ok := c.blocks[id][key]; ok {
+		header := make([]byte, headerLen)
+		copy(header, data[:headerLen])
+		entry := (*entry)(unsafe.Pointer(&header[0]))
+		if entry.keyType != keyType {
+			return nil, incorrectSelectKeyType
+		}
+		now := time.Now().Unix()
+
+		if entry.ttl > 0 && entry.ttl <= uint64(now) {
+			c.locks[id].RUnlock()
+			c.locks[id].Lock()
+			delete(c.blocks[id], key)
+			c.locks[id].Unlock()
+			return nil, notFoundErr
+		}
+		out := make([]byte, entry.length)
+		copy(out, data[headerLen:])
+		c.locks[id].RUnlock()
+		return out, nil
+	} else {
+		c.locks[id].RUnlock()
+		return nil, notFoundErr
+	}
+}
+
+func (c *simpleCacheDb) set(key string, keyType uint8, ttl int64, value []byte) error {
+	elem := &entry{uint64(len(value)), getTTL(ttl), keyType}
+	data := *(*[headerLen]byte)(unsafe.Pointer(elem))
+
+	nVal := make([]byte, len(value))
+	copy(nVal, value)
+
+	id := blockByKey(key)
+	c.locks[id].Lock()
+	c.blocks[id][key] = append(data[:], nVal...)
+	c.locks[id].Unlock()
+	return nil
+}
+
+func (c *simpleCacheDb) setList(key string, keyType uint8, ttl int64, values [][]byte) error {
+	if len(values) > maxListElemennts {
+		return tooMatchListElementsErr
+	}
+	off := (len(values) * 2) + 2
+	lenBuff := off
+	for _, val := range values {
+		lenBuff += len(val)
+	}
+
+	if keyType == keyDict {
+		lenBuff += len(values) * 2
+	}
+
+	buff := make([]byte, lenBuff)
+	//write counnt elements in record header
+	data := sliceUnsafeConvert(uint16(len(values)))
+	copy(buff[:2], data[:])
+
+	for i, val := range values {
+		elemLen := len(val)
+		if keyType == keyDict {
+			elemLen += 2
+		}
+		//write elem length in record header
+		data := sliceUnsafeConvert(uint16(elemLen))
+		copy(buff[i*2+2:(i*2)+4], data[:])
+		if keyType == keyDict {
+			//additional separator index in record start
+			sepIndex := uint16(bytes.Index(val, dictionarySeparator))
+			sepData := sliceUnsafeConvert(uint16(sepIndex))
+			copy(buff[off:off+2], sepData[:])
+			copy(buff[off+2:off+elemLen], val)
+		} else {
+			copy(buff[off:off+elemLen], val)
+		}
+		off += elemLen
+	}
+
+	return c.set(key, keyType, ttl, buff)
+}
+
+func (c *simpleCacheDb) SetList(key string, ttl int64, values [][]byte) error {
+	return c.setList(key, keyList, ttl, values)
+}
+
+func (c *simpleCacheDb) Get(key string) ([]byte, error) {
+	return c.get(key, keyString)
+}
+
+func (c *simpleCacheDb) Set(key string, ttl int64, value []byte) error {
+	return c.set(key, keyString, ttl, value)
+}
+
+func (c *simpleCacheDb) getList(key string, keyType uint8) ([][]byte, error) {
+	data, err := c.get(key, keyType)
+	if err != nil {
+		return nil, err
+	}
+
+	elemCount := uint16UnsafeConvert(data)
+	out := make([][]byte, elemCount)
+
+	off := (elemCount * 2) + 2
+	var i uint16
+	for i = 0; i < elemCount; i++ {
+		elemLen := uint16UnsafeConvert(data[i*2+2: i*2+4])
+		out[i] = make([]byte, elemLen)
+		copy(out[i], data[off:off+elemLen])
+		off += elemLen
+	}
+
+	return out, nil
+}
+
+func (c *simpleCacheDb) GetList(key string) ([][]byte, error) {
+	return c.getList(key, keyList)
+}
+
+func (c *simpleCacheDb) GetListElement(key string, position uint16) ([]byte, error) {
+	data, err := c.get(key, keyList)
+
+	if err != nil {
+		return nil, err
+	}
+
+	off, elemLen, err := getElemByPosition(data, position)
+	if err != nil {
+		return nil, err
+	}
+	return data[off: off+elemLen], nil
+}
+
+func (c *simpleCacheDb) SetDict(key string, ttl int64, values dictionary) error {
+	for _, val := range values {
+		if bytes.Index(val, dictionarySeparator) == -1 {
+			return incorrectDictElementErr
+		}
+	}
+	sort.Sort(values)
+
+	return c.setList(key, keyDict, ttl, values)
+}
+
+func (c *simpleCacheDb) GetDict(key string) ([][]byte, error) {
+	return c.getList(key, keyDict)
+}
+
+func (c *simpleCacheDb) GetDictElement(key string, dictKey []byte) ([]byte, error) {
+	data, err := c.get(key, keyDict)
+
+	if err != nil {
+		return nil, err
+	}
+
+	elemCount := int(uint16UnsafeConvert(data))
+
+	//custom binary search
+	i := sort.Search(elemCount, func(position int) bool {
+		off, elemLen, _ := getElemByPosition(data, uint16(position))
+
+		separatorPosition := uint64(uint16UnsafeConvert(data[off: off+elemLen]))
+		return bytes.Compare(data[off+2:off+2+separatorPosition], dictKey) >= 0
+	})
+
+	if i < elemCount {
+		off, elemLen, _ := getElemByPosition(data, uint16(i))
+		separatorPosition := uint64(uint16UnsafeConvert(data[off: off+elemLen]))
+		if bytes.Equal(data[off+2:off+2+separatorPosition], dictKey) {
+			return data[off+2+1+separatorPosition: off+elemLen], nil
+		} else {
+			return nil, notFoundErr
+		}
+	} else {
+		return nil, notFoundErr
+	}
+}
+
+func uint16UnsafeConvert(data []byte) uint16 {
+	elemCountData := make([]byte, 2)
+	copy(elemCountData, data[0:2])
+	return *(*uint16)(unsafe.Pointer(&elemCountData[0]))
+}
+
+func sliceUnsafeConvert(val uint16) []byte {
+	return (*(*[2]byte)(unsafe.Pointer(&val)))[:]
+}
+
+//get element by position in byte slice
+func getElemByPosition(data []byte, position uint16) (uint64, uint64, error) {
+	elemCount := uint16UnsafeConvert(data)
+	if position >= elemCount {
+		return 0, 0, notFoundErr
+	}
+
+	off := uint64(elemCount)*2 + 2
+	var i uint16
+	for i = 0; i < position; i++ {
+		off += uint64(uint16UnsafeConvert(data[i*2+2: i*2+4]))
+	}
+
+	elemLen := uint16UnsafeConvert(data[i*2+2: i*2+4])
+	return off, uint64(elemLen), nil
+}
---
+++ kv/simple_cache_test.go
@@ -0,0 +1,373 @@
+package kv
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGetSetSimpleCache(t *testing.T) {
+	cache := newSimpleCacheDb()
+
+	val := "baz"
+	cache.Set("foo", 0, []byte(val))
+
+	data, err := cache.Get("foo")
+	if err != nil {
+		t.Fatal("Get key Error", err.Error())
+	}
+
+	if string(data) != val {
+		t.Fatal("Incorrect value", "expected", val, "got", string(data))
+	}
+}
+
+func TestGetSetTTLSimpleCache(t *testing.T) {
+	cache := newSimpleCacheDb()
+
+	val := "baz"
+	cache.Set("foo", 2, []byte(val))
+	time.Sleep(time.Second * 3)
+
+	data, err := cache.Get("foo")
+	if err == nil {
+		t.Fatal("Expected Error", "got nil", "Data", data)
+	}
+
+	if err != notFoundErr {
+		t.Fatal("Incorrect Error", "expected", notFoundErr, "got", err)
+	}
+}
+
+func TestGetSetListCache(t *testing.T) {
+	cache := newSimpleCacheDb()
+
+	val := [][]byte{
+		[]byte("baz"),
+		[]byte("bar"),
+		[]byte("foo"),
+		[]byte("foobaz"),
+		[]byte("foo_bar"),
+		[]byte("hello"),
+	}
+
+	cache.SetList("foo", 0, val)
+
+	data, err := cache.GetList("foo")
+	if err != nil {
+		t.Fatal("Get key Error", err.Error())
+	}
+
+	for i, elem := range data {
+		if string(elem) != string(val[i]) {
+			t.Fatal("Incorrect element", "expected", string(val[i]), "got", string(elem))
+		}
+	}
+
+	positions := []uint16{3, 0, 2, 5}
+	for _, pos := range positions {
+		//t.Log(pos)
+		elem, err := cache.GetListElement("foo", pos)
+		if err != nil {
+			t.Fatal("Get key Error", err.Error(), pos)
+		}
+
+		if string(elem) != string(val[pos]) {
+			t.Fatal("Incorrect element", "expected", string(val[pos]), "got", string(elem))
+		}
+	}
+
+	_, err = cache.GetListElement("foo", 6)
+	if err != notFoundErr {
+		t.Fatal("Expected Error", notFoundErr.Error(), "got", err)
+	}
+}
+
+func TestGetSetDictCache(t *testing.T) {
+	cache := newSimpleCacheDb()
+
+	val := [][]byte{
+		[]byte("foo:baz"),
+		[]byte("fooo:baz"),
+		[]byte("baz:foobaz"),
+		[]byte("zbaz:world"),
+		[]byte("bar:BAR"),
+		[]byte("c:hello"),
+		[]byte("d:hello"),
+	}
+
+	cache.SetDict("foo", 0, val)
+
+	data, err := cache.GetDict("foo")
+	if err != nil {
+		t.Fatal("Get key Error", err.Error())
+	}
+
+	for i, elem := range data {
+		if string(elem[2:]) != string(val[i]) {
+			t.Fatal("Incorrect element", "expected", string(val[i]), "got", string(elem[2:]))
+		}
+	}
+
+	indexSearch := map[string]string{
+		"zbaz": "world",
+		"baz":  "foobaz",
+		"fooo": "baz",
+		"c":    "hello",
+		"bar":  "BAR",
+	}
+	for key, val := range indexSearch {
+		elem, err := cache.GetDictElement("foo", []byte(key))
+		if err != nil {
+			t.Fatal("Get key Error: ", err.Error())
+		}
+
+		if string(elem) != val {
+			t.Fatal("Incorrect element", "expected", val, "got", string(elem))
+		}
+	}
+
+	_, err = cache.GetDictElement("foo", []byte("hello"))
+	if err != notFoundErr {
+		t.Fatal("Expected Error", notFoundErr.Error(), "got", err)
+	}
+}
+
+func BenchmarkGetParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	cache.Set("foo", 0, []byte("bar"))
+	cache.Set("baz", 0, []byte("barbaz"))
+	cache.Set("bar", 0, []byte("foobar"))
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Get("foo")
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Get("baz")
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Get("bar")
+		}
+	})
+}
+
+func BenchmarkSetParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Set("foo", 0, []byte("bar"))
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Set("baz", 0, []byte("barbaz"))
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Set("bar", 0, []byte("foobar"))
+		}
+	})
+}
+
+func BenchmarkSetListParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.SetList("foo", 0, [][]byte{
+				[]byte("baz"),
+				[]byte("baz"),
+				[]byte("foo"),
+				[]byte("foobaz"),
+			})
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.SetList("foobaz", 0, [][]byte{
+				[]byte("baz"),
+				[]byte("baz"),
+				[]byte("foo"),
+				[]byte("foobaz"),
+			})
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.SetList("bar", 0, [][]byte{
+				[]byte("baz"),
+				[]byte("baz"),
+				[]byte("foo"),
+				[]byte("foobaz"),
+			})
+		}
+	})
+}
+
+func BenchmarkGetListParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	cache.SetList("foobaz", 0, [][]byte{
+		[]byte("baz"),
+		[]byte("baz"),
+		[]byte("foo"),
+		[]byte("foobaz"),
+	})
+
+	cache.SetList("bar", 0, [][]byte{
+		[]byte("baz"),
+		[]byte("baz"),
+		[]byte("foo"),
+		[]byte("foobaz"),
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetList("foobaz")
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Get("bar")
+		}
+	})
+}
+
+func BenchmarkGetListElemParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	cache.SetList("bar", 0, [][]byte{
+		[]byte("baz"),
+		[]byte("baz"),
+		[]byte("foo"),
+		[]byte("foobaz"),
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetListElement("bar", 1)
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetListElement("bar", 2)
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetListElement("bar", 20)
+		}
+	})
+}
+
+func BenchmarkSetDictParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.SetDict("foo", 0, [][]byte{
+				[]byte("baz:bar"),
+				[]byte("baz:world"),
+				[]byte("foo:hello"),
+				[]byte("foobaz:test"),
+			})
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.SetDict("foobaz", 0, [][]byte{
+				[]byte("baz:bar"),
+				[]byte("baz:world"),
+				[]byte("foo:hello"),
+				[]byte("foobaz:test"),
+			})
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.SetDict("bar", 0, [][]byte{
+				[]byte("baz:bar"),
+				[]byte("baz:world"),
+				[]byte("foo:hello"),
+				[]byte("foobaz:test"),
+			})
+		}
+	})
+}
+
+func BenchmarkGetDictParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	cache.SetList("foobaz", 0, [][]byte{
+		[]byte("baz:bar"),
+		[]byte("baz:world"),
+		[]byte("foo:hello"),
+		[]byte("foobaz:test"),
+	})
+
+	cache.SetList("bar", 0, [][]byte{
+		[]byte("baz:bar"),
+		[]byte("baz:world"),
+		[]byte("foo:hello"),
+		[]byte("foobaz:test"),
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetList("foobaz")
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.Get("bar")
+		}
+	})
+}
+
+func BenchmarkGetDictElemParallel(b *testing.B) {
+	cache := newSimpleCacheDb()
+
+	cache.SetList("bar", 0, [][]byte{
+		[]byte("baz:bar"),
+		[]byte("baz:world"),
+		[]byte("foo:hello"),
+		[]byte("foobaz:test"),
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetDictElement("bar", []byte("foo"))
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetDictElement("bar", []byte("baz"))
+		}
+	})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cache.GetDictElement("bar", []byte("hello"))
+		}
+	})
+}
